e859f72c6155ac1255b6245611e648c73a7af644
om-khorwal <amankhorwal2002@gmail.com>
2024-01-12 00:46:24 +0530

    added reponsivenss

--- src/app/Components/Heroslide.js
+++ src/app/Components/Heroslide.js
@@ -18,10 +18,14 @@ const Heroslide = () => {
     <>
 
       <div id='home' className=' relative h-screen w-full' >
-        <img className="absolute w-[50%] h-[50%] left-[30%] top-[8.5%] object-cover"
+        
+        {/* <div className="flex w-full h-[60%] justify-center ">
+        <img className="w-16 md:w-32 lg:w-48 "
           alt=""
           src="/img.png"
-        />
+          />
+        </div> */}
+        
         <img className="dot fixed w-20 h-20 z-30 right-10 top-10   "
           alt=""
           src="/dot.png"
@@ -41,74 +45,74 @@ const Heroslide = () => {
         </div> */}
 
 
-        <div className=' z-10 fixed text-black font-bold bg-gradient-to-b from-[#000000] to-[#0652c3] items-center justify-center  w-20 h-[35%] top-[25%] m-10 rounded-full uppercase shadow-xl shadow-white'>
+        <div className='sidebar z-10 fixed text-black font-bold bg-gradient-to-b from-[#000000] to-[#0652c3] items-center justify-center  w-20 h-[35%] top-[25%] m-8 rounded-full uppercase shadow-xl shadow-white'>
 
-          <div className="absolute text-white h-full gap-5 w-full flex flex-col justify-center items-center  ">
+          <div className="  text-white h-full gap-5 w-full flex flex-col justify-center items-center py-5  ">
 
-            <Link 
-            to='home'
-            activeClass='active'
-            smooth={true}
-            spy={true} 
-            className=" text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold">
+            <Link
+              to='home'
+              activeClass='active'
+              smooth={true}
+              spy={true}
+              className=" innersidebar text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold">
               <FaHome />
             </Link>
 
-            <Link 
-            to='skills' 
-            activeClass='active'
-            smooth={true}
-            spy={true} 
-            className=" text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold" >
-            <GrProjects />
+            <Link
+              to='skills'
+              activeClass='active'
+              smooth={true}
+              spy={true}
+              className=" innersidebar text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold" >
+              <GrProjects />
             </Link>
 
-            <Link 
-            to='projects' 
-            activeClass='active'
-            smooth={true}
-            spy={true} 
-            className=" text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold">
-            <LiaLaptopCodeSolid />
+            <Link
+              to='projects'
+              activeClass='active'
+              smooth={true}
+              spy={true}
+              className=" innersidebar text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold">
+              <LiaLaptopCodeSolid />
             </Link>
 
-            <Link 
-            to='about' 
-            activeClass='active'
-            smooth={true}
-            spy={true} 
-            className=" text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold">
+            <Link
+              to='about'
+              activeClass='active'
+              smooth={true}
+              spy={true}
+              className=" innersidebar text-3xl font-medium hover:-translate-y-1 hover:scale-125 duration-200 hover:font-bold">
               <FcAbout />
             </Link>
 
 
           </div>
         </div>
-        <div className=' flex justify-center items-center z-10 w-20 h-20 bg-transparent fixed top-[75%] right-[3%] rounded-full opacity-100 shadow-inner shadow-white hover:hover:scale-125 hover:bg-white duration-500'>
+        <div className='file flex justify-center items-center z-10 w-20 h-20 bg-transparent fixed top-[75%] right-[3%] rounded-full opacity-100 shadow-inner shadow-white hover:hover:scale-125 hover:bg-white duration-500'>
           <a className="flex justify-center items-center" href="https://drive.google.com/file/d/1x0vLCRtqoZxFi0jfSS8NkNzIyRYq52qV/view?usp=sharing" target="_blank">
-          <FaFileAlt className='absolute w-[50%] h-[60%] text-blue-400 ' />
+            <FaFileAlt className='absolute w-[50%] h-[60%] text-blue-400 ' />
           </a>
         </div>
 
 
-        <div className=" text-white absolute px-[18%] h-[15.3%] w-[100%] top-[35%] font-semibold  inline-block [text-shadow-[10px_4px_5px_rgba(0,0,0.25)] opacity-1">
-          <p id="head" className=" text-[3rem]  drop-shadow-[0px_10px_0px_rgba(0,0,0)]">Hi,</p>
+        <div className=" herotext  text-white relative px-[20%] h-[15.3%] w-full top-[35%] font-semibold  inline-block [text-shadow-[10px_4px_5px_rgba(0,0,0.25)] opacity-1">
+          <p id="head" className="head1 text-[3rem]  drop-shadow-[0px_10px_0px_rgba(0,0,0)]">Hi,</p>
           <div className='flex gap-3'>
             <p className=" head1 text-[3rem]  drop-shadow-[0px_10px_0px_rgba(0,0,0)]">I'm</p>
             <p className=" head1 text-[3rem] text-[#00a3ff] drop-shadow-[0px_10px_0px_rgba(0,0,0)] hover:scale-150">Om</p>
           </div>
-          <p id="fhead" className=" text-[5rem] drop-shadow-[5px_10px_2px_rgba(0,0,0)] ">FRONT-END DEVELOPER</p>
+          <p id="fhead" className=" flex w-full text-[5rem] drop-shadow-[5px_10px_2px_rgba(0,0,0)] ">FRONT-END DEVELOPER</p>
         </div>
 
-        <div className="absolute flex h-[5%] w-[10%] z-20 justify-center items-center top-[70%] left-[70%] bg-white shadow-[10px_4px_5px_rgba(0,0,0.25)] hover:bg-blue-900 duration-300 hover:h-[7%]" >
+        <div className="contact relative  h-[5%] w-[10%] z-20 justify-center items-center top-[40%] left-[69%] bg-white shadow-[10px_4px_5px_rgba(0,0,0.25)] hover:bg-blue-900 duration-300 hover:h-[7%]" >
           <button className="  h-full w-full text-[1.7rem] font-exo bg-transparent text-black  inline-block hover:text-white duration-500">
             <a href="mailto:amankhorwal2002@gmail.com" target="_blank" rel="noopener noreferrer">
-            Contact Me
-           </a>
+              Contact Me
+            </a>
           </button>
         </div>
 
-        <div className='flex h-full w-full absolute bottom-5 '>
+        <div className='  flex h-full w-full absolute bottom-5 '>
 
           <a href="linkedin.com" target="_blank" rel="noreferrer">
             <FaLinkedin className=" text-white absolute rounded-lg h-[5%] w-fit bottom-5  left-[20%] max-w-full  max-h-full object-cover z-20 hover:text-blue-900 hover:shadow-2xl hover:shadow-white hover:scale-125 duration-300 hover:bg-white " />
--- src/app/Components/Projects.js
+++ src/app/Components/Projects.js
@@ -11,35 +11,33 @@ const Projects = () => {
                 </img>
 
                 {/* <h2 className='flex absolute text-5xl top-[10%] w-[18%] h-[10%] font-bold items-center justify-center  hover:bg-white hover:text-black rounded-md'>PROJECTS</h2> */}
-                <div className=' absolute w-1/2 h-32 top-[20%]  '>
+                <div className=' absolute w-1/2 h-32 top-[5%]  '>
                     <h4 className=' absolute text-3xl m-10'>Portfolio website:-
-                        <p>
+                        <p className='text-xl font-exo'>
                             Developed my own portfolio website by using React and Tailwind Css
                         </p>
                     </h4>
                 </div>
-                <div className=' absolute w-1/2 h-32 top-[20%]  '>
+                <div className=' absolute w-1/2 h-32 top-[15%]  '>
                     <h4 className=' absolute text-3xl m-10'>Docs webpage:-
-                        <p>
+                    <p className='text-xl font-exo'>
                             Created Notes webpage with dragging feature
                         </p>
                     </h4>
                 </div>
-                <div className=' absolute w-1/2 h-32 top-[20%]  '>
+                <div className=' absolute w-1/2 h-32 top-[25%]  '>
                     <h4 className=' absolute text-3xl m-10'>Todo-list:-
-                        <p>
+                    <p className='text-xl font-exo'>
                         Normal Todo list to add task
                         </p>
                     </h4>
                 </div>
 
-                <div className=' absolute w-1/2 h-36 top-[40%]'>
+                <div className=' absolute w-1/2 h-36 top-[35%]'>
                     <h4 className=' absolute text-3xl m-10  '>MARWADI BAWARCHI:-
-
-                        <a className='text-blue-400 font-semibold' target='blank' href='https://om-khorwal.github.io/Marwadi_Bawarchi/'> Visit here...</a>
-                        <p className='flex'>
+                    <p className='text-xl font-exo'>
                             A web page which is built using html-css-js and Gsap (Javascript library) is used to create animation.
-                            Website Url https://om-khorwal.github.io/Marwadi_Bawarchi/                   
+                        <a className='text-blue-400 font-semibold' target='blank' href='https://om-khorwal.github.io/Marwadi_Bawarchi/'> Visit here...</a>
                         </p>
                     </h4>
                 </div>
