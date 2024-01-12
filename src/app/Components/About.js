59e54a24d6aa0e009421f6fced615fc6bd6945d9
om-khorwal <amankhorwal2002@gmail.com>
2024-01-12 16:25:08 +0530

    added reponsivenss

--- src/app/Components/About.js
+++ src/app/Components/About.js
@@ -3,9 +3,9 @@ import React from 'react'
 const About = () => {
     return (
         <div id='about' className=' relative flex text-3xl text-white w-full h-screen justify-around '>
-            <div className=' absolute flex justify-around w-full h-[80%] top-[10%]'>
-                <h2 className='  text-5xl justify-center items-center  text-center '>About
-                    <div>
+            <div className='about absolute flex justify-around w-full h-[80%] top-[10%]'>
+                <h2 className=' text-5xl justify-center items-center  text-center '>About
+                    <div className='aboutp'>
                         <h4 className='text-xl w-72 text-center mt-10 '>
                             An engineering student who has always had a passion for technology and its applications in various fields. Throughout my academic journey, I have strived to explore different avenues and gain diverse experiences. I have experience in web development and also worked in different domains too. As a technophile, I am constantly seeking new challenges and opportunities to expand my skill set.
                         </h4>
@@ -13,7 +13,7 @@ const About = () => {
                 </h2>
                 <div className='absolute w-1 h-full bg-white shadow-xl shadow-white'></div>
                 <h2 className=' text-5xl justify-center items-center  text-center '>Contact
-                    <div className=''>
+                    <div className='details'>
                         <h4 className='text-xl w-full text-center mt-10 '>
                             Email: amankhorwal2002@gmail.com
                         </h4>
